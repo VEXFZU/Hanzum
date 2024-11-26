6e23a5f85cf86606d919ed2d22a2d5d0004873be
Vincent <35026336+azaraks@users.noreply.github.com>
2024-11-26 15:57:20 +0900

    Processes only one-quarter of the maximum token limit per inference.

--- api/model/t5.py
+++ api/model/t5.py
@@ -3,22 +3,52 @@ from transformers import T5ForConditionalGeneration, AutoTokenizer
 
 _model_name_or_path = os.environ.get("VXFZ_TRANSLATER_MODEL_T5_NAME", "azaraks/t5-xlarge-ko-kb")
 _max_length = int(os.environ.get("VXFZ_TRANSALTER_MODEL_T5_MAX_LENGTH", "256"))
+_max_input_length = _max_length >> 2
 
 model = T5ForConditionalGeneration.from_pretrained(_model_name_or_path).to('cuda')
 tokenizer = AutoTokenizer.from_pretrained(_model_name_or_path)
 
 
+def split_sentence_with_tokenizer(sentence):
+    words = sentence.split()
+    result = []
+    start, end = 0, len(words)
+
+    while start < len(words):
+        low, high = start, end
+        while low < high:
+            mid = (low + high) // 2
+            chunk = " ".join(words[start:mid + 1])
+            tokenized = tokenizer(chunk, return_tensors='pt', max_length=_max_length, truncation=True)
+            if tokenized.input_ids.shape[1] <= _max_input_length:
+                low = mid + 1
+            else:
+                high = mid
+        result.append(" ".join(words[start:low]))
+        start = low
+
+    return result
+
+
 def translate(text):
-    input_text = f'translate Korean to Braille: "{text}"'
-    inputs = tokenizer(input_text,
-                       return_tensors="pt",
-                       max_length=_max_length,
-                       truncation=True,
-                       ).to('cuda')
-    outputs = model.generate(**inputs,
-                             max_length=_max_length,
-                             num_beams=4,
-                             early_stopping=True,
-                             )
-    output_text = tokenizer.decode(outputs[0], skip_special_tokens=False)
-    return output_text[6:-5]  # removes `<pad>"`(6 chars) from the start and `"</s>`(5 chars) from the end.
+    chunks = split_sentence_with_tokenizer(text)
+    output_texts = []
+    for chunk in chunks:
+        print(chunk)
+        instruction = f'translate Korean to Braille: "{chunk}"'
+        inputs = tokenizer(instruction,
+                           return_tensors='pt',
+                           max_length=_max_length,
+                           truncation=True,
+                           ).to('cuda')
+
+        outputs = model.generate(**inputs,
+                                 max_length=_max_length,
+                                 num_beams=4,
+                                 early_stopping=True)
+
+        output_text = tokenizer.decode(outputs[0], skip_special_tokens=False)
+        print(output_text)
+        output_texts.append(output_text[6:-5])
+    output_text = ' '.join(output_texts)
+    return output_text
